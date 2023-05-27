daab0cb881775704e30c4dd945bf530736464308
Meghna Mitra <114507725+MEGHNAMITRA@users.noreply.github.com>
2023-05-27 08:18:07 +0530

    Add files via upload

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,64 @@
+import { Link } from "react-router-dom";
+import "./sidebar.css";
+
+export default function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="sidebarItem">
+        <span className="sidebarTitle">ABOUT ME</span>
+        <img
+          src="https://images.ctfassets.net/pdf29us7flmy/3KxEs7y5tDsw6ptxHLCN8w/c6cec487ea89d6f8fb0fec3f59eeca4a/WomanAtLaptop.png?w=720&q=100&fm=jpg"
+          alt=""
+        />
+        <p>
+          Laboris sunt aute cupidatat velit magna velit ullamco dolore mollit
+          amet ex esse.Sunt eu ut nostrud id quis proident.
+        </p>
+      </div>
+      <div className="sidebarItem">
+        <span className="sidebarTitle">CATEGORIES</span>
+        <ul className="sidebarList">
+          <li className="sidebarListItem">
+            <Link className="link" to="/posts?cat=Life">
+              Life
+            </Link>
+          </li>
+          <li className="sidebarListItem">
+            <Link className="link" to="/posts?cat=Music">
+              Music
+            </Link>
+          </li>
+          <li className="sidebarListItem">
+            <Link className="link" to="/posts?cat=Sport">
+              Sport
+            </Link>
+          </li>
+          <li className="sidebarListItem">
+            <Link className="link" to="/posts?cat=Style">
+              Style
+            </Link>
+          </li>
+          <li className="sidebarListItem">
+            <Link className="link" to="/posts?cat=Tech">
+              Tech
+            </Link>
+          </li>
+          <li className="sidebarListItem">
+            <Link className="link" to="/posts?cat=Cinema">
+              Cinema
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <div className="sidebarItem">
+        <span className="sidebarTitle">FOLLOW US</span>
+        <div className="sidebarSocial">
+          <i className="sidebarIcon fab fa-facebook-square"></i>
+          <i className="sidebarIcon fab fa-instagram-square"></i>
+          <i className="sidebarIcon fab fa-pinterest-square"></i>
+          <i className="sidebarIcon fab fa-twitter-square"></i>
+        </div>
+      </div>
+    </div>
+  );
+}
